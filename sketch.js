77f15971a93d212577b920cdea5a57eb3bc8999d
DasWelch <35710104+DasWelch@users.noreply.github.com>
2021-04-06 15:26:21 -0400

    have rocket drawing and p5 play in lib, good start

--- sketch.js
+++ sketch.js
@@ -8,10 +8,12 @@ let border;
 
 let angle = 0;
 
+let fh = 50;
+
 function updateContainer() {
-  container = select('#sketchContainer');
-  w = parseFloat(getComputedStyle(container.elt).getPropertyValue('width'));
-  h = parseFloat(getComputedStyle(container.elt).getPropertyValue('height'));
+  container = select("#sketchContainer");
+  w = parseFloat(getComputedStyle(container.elt).getPropertyValue("width"));
+  h = parseFloat(getComputedStyle(container.elt).getPropertyValue("height"));
 }
 
 function windowResized() {
@@ -19,60 +21,33 @@ function windowResized() {
   resizeCanvas(w, h);
 }
 
+let ship;
+
 function setup() {
   updateContainer();
-  canvas = createCanvas(w, h, WEBGL);
-  smooth();
+  canvas = createCanvas(w, h);
   canvas.parent("#sketchContainer");
+
+  // console.log(height)
+
+  createSprite(width / 2, height - 50 / 2, w, 50);
+
+  ship = new Rocket();
 }
 
 function draw() {
-  translate(-width / 2, -height / 2);
+  background(255, 255, 255);
 
-  background('#fffff8');
-
-  strokeWeight(1);
-  stroke('#222831');
-  fill('#222831');
-
-  stroke('#c7b198');
-  fill('#c7b198');
-  ellipse(mouseX, mouseY, 50, 50);
-
-  // drawings
-  push();
-  stroke('#c7b198');
-  strokeWeight(2);
-  translate(width / 2, height / 2);
-  noFill();
-  rotateY(angle);
-  rotateX(angle);
-  box(100, 100);
-
-  stroke('#321f28');
-  fill('#321f28');
-  // noFill();
-  sphere(25);
-
-  pop();
-
-  // border
-  stroke('#222831');
-  noFill();
-  strokeWeight(5);
-  rectMode("corners");
-  rect(0, 0, width, height);
-  angle += 0.01;
-
+  drawSprites();
   document.getElementById("fps").innerHTML = frameRate().toFixed(2);
 }
 
-function colorAlpha(aColor, alpha) {
-  // allows usage of HEX colors with alpha
-  const c = color(aColor);
-  let a = alpha;
-  if (alpha <= 0.1) {
-    a = 0.1;
+class Rocket {
+  constructor() {
+    this.rocket = createSprite(width / 2, height / 2, 10, 10);
   }
-  return color('rgba(${[red(c), green(c), blue(c), a].join(', ')})');
+
+  run() {
+    draw(this.rocket);
+  }
 }
